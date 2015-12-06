55558e87d28d99e6556c7c366379428a3306d4a7
Lourens Elzinga <lourens_elzinga@hotmail.com>
2015-12-06 15:33:15 +0100

    Minor optimizations for day 5.

--- days/day5/day5.cpp
+++ days/day5/day5.cpp
@@ -53,7 +53,7 @@ bool isReallyNice(const std::string &str)
     bool hasRepeat = false;
 
     // Search repeats with a single character inbetween
-    for (int i = 0, n = str.size() - 2; i != n; ++i)
+    for (int i = 0, n = str.size() - 2; i < n; ++i)
     {
         if (str[i] == str[i + 2])
         {
@@ -66,21 +66,13 @@ bool isReallyNice(const std::string &str)
         return false;
 
     // Search non-overlapping pairs
-    for (int i = 0, n = str.size() - 1; i != n; ++i)
+    for (int i = 0, n = str.size() - 1; i < n; ++i)
     {
         char pattern[2] = { str[i], str[i + 1] };
 
-        for (int j = 0, m = str.size() - 1; j != m; ++j)
-        {
-            // Skip area around search pattern (would be an overlapped match)
-            if (j > i - 2 && j < i + 2)
-                continue;
-
+        for (int j = i + 2, m = str.size() - 1; j < m; ++j)
             if (str[j] == pattern[0] && str[j + 1] == pattern[1])
-            {
                 return true;
-            }
-        }
     }
 
     return false;
