bd4b375ee22215bf7550501d71fb86b1509ec78f
Lourens Elzinga <lourens_elzinga@hotmail.com>
2015-12-25 17:00:33 +0100

    Altered day 25 to use modular exponentiation.

--- days/day25/day25.cpp
+++ days/day25/day25.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
 #include <regex>
 
-bool getInput(unsigned &row, unsigned &col)
+using ull = unsigned long long;
+// Calculates initial * base ^ exponent % modulus
+ull modexp(unsigned base, ull exponent, ull modulus, ull initial = 1)
 {
-    static const std::regex inputRegex("To continue, please consult the code grid in the manual.  Enter the code at row (\\d+), column (\\d+).");
-    std::string line;
-    std::smatch match;
-    getline(std::cin, line);
-
-    if (regex_match(line, match, inputRegex))
+    ull result = initial;
+    while (exponent)
     {
-        row = stoul(match[1]), col = stoul(match[2]);
-        return true;
+        if (exponent & 1)
+            result = (result * base) % modulus;
+        exponent >>= 1;
+        base = (base * base) % modulus;
     }
 
-    return false;
+    return result;
 }
 
 int main(int, char **)
 {
-    unsigned row, col;
-    if (!getInput(row, col))
+    static constexpr ull initial = 20151125, multiplicand = 252533, modulus = 33554393;
+    static const std::regex inputRegex("To continue, please consult the code grid in the manual.  Enter the code at row (\\d+), column (\\d+).");
+
+    std::string line;
+    std::smatch match;
+    getline(std::cin, line);
+    if (!regex_match(line, match, inputRegex))
     {
         std::cout << "Could not parse input." << std::endl;
         return 1;
     }
 
-    unsigned long long index = 0;
+    unsigned row = stoul(match[1]), col = stoul(match[2]);
+
+    ull index = 0;
     for (unsigned i = col + row - 1; i > 0; --i)
         index += i;
-    index -= row - 1;
+    index -= row;
 
-
-    unsigned long long code = 20151125;
-    while (--index)
-        code = code * 252533 % 33554393;
-
-    std::cout << "Code: " << code << std::endl;
+    std::cout << "Code: " << modexp(multiplicand, index, modulus, initial) << std::endl;
 }
